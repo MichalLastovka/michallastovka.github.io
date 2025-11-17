4938a5369499deabd0f0b73f42d43f054e3252b1
Michal Lastovka <lasmich@protonmail.com>
2025-11-17 08:06:36 +0100

    favicon added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {AppThemeContextProvider} from "./context/ThemeContextProvider.tsx";
 import {CssBaseline} from "@mui/material";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import WebLayout from "./components/WebLayout.tsx";
 import {HomePage} from "./pages/HomePage.tsx";
 import {Portolio} from "./pages/Portfolio.tsx";
@@ -14,32 +14,32 @@ function App() {
         // Apply smooth scrolling to the html element via JS
         document.documentElement.style.scrollBehavior = 'smooth';
     }, []);
-  return (
-      <>
-          <AppThemeContextProvider>
-              <CssBaseline/> {/* MUI's baseline styles */}
-              <BrowserRouter>
-                  <Routes>
-                      <Route element={< WebLayout/>}>
-                          <Route path={"/"} element={<HomePage/>}></Route>
-                      </Route>
-                      <Route element={< WebLayout/>}>
-                          <Route path={"/portfolio"} element={<Portolio/>}></Route>
-                      </Route>
-                      <Route element={< WebLayout/>}>
-                          <Route path={"/portfolio/:appName"} element={<AppDetail/>}></Route>
-                      </Route>
-                      <Route element={< WebLayout/>}>
-                          <Route path={"/about"} element={<About/>}></Route>
-                      </Route>
-                      <Route element={< WebLayout/>}>
-                          <Route path={"/contacts"} element={<Contacts/>}></Route>
-                      </Route>
-                  </Routes>
-              </BrowserRouter>
-          </AppThemeContextProvider>
-      </>
-  )
+    return (
+        <>
+            <AppThemeContextProvider>
+                <CssBaseline/> {/* MUI's baseline styles */}
+                <HashRouter>
+                    <Routes>
+                        <Route element={< WebLayout/>}>
+                            <Route path={"/"} element={<HomePage/>}></Route>
+                        </Route>
+                        <Route element={< WebLayout/>}>
+                            <Route path={"portfolio"} element={<Portolio/>}></Route>
+                        </Route>
+                        <Route element={< WebLayout/>}>
+                            <Route path={"portfolio/:appName"} element={<AppDetail/>}></Route>
+                        </Route>
+                        <Route element={< WebLayout/>}>
+                            <Route path={"about"} element={<About/>}></Route>
+                        </Route>
+                        <Route element={< WebLayout/>}>
+                            <Route path={"contacts"} element={<Contacts/>}></Route>
+                        </Route>
+                    </Routes>
+                </HashRouter>
+            </AppThemeContextProvider>
+        </>
+    )
 }
 
 export default App
